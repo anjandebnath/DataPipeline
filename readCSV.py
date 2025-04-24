6b901c8ddd8b05559ce97ac9b88c470fdf1cd321
Anjan Debnath <anjan.debnath@solshare.com>
2025-04-24 16:29:52 +0600

    run poetry inside virtualbox vagrant and loadCSV python

--- readCSV.py
+++ readCSV.py
@@ -1,7 +1,10 @@
 import csv
 
+# Mac Env
+# with open('/Users/anjandebnath/Documents/DataPipeline/data.csv') as f:
 
-with open('/Users/anjandebnath/Documents/DataPipeline/data.csv') as f:
+# Ubuntu Env
+with open('/home/vagrant/data/data.csv') as f:
 	myreader=csv.DictReader(f)
 	headers=next(myreader)
 	for row in myreader:
